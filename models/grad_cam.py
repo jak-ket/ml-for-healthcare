0a6eb2a318753676acf724f6c366f36ff855fb61
Marvin Lob <marvin.lob@quantco.com>
2024-03-27 11:31:21 +0100

    made more function for better readability

--- models/grad_cam.py
+++ models/grad_cam.py
@@ -38,15 +38,13 @@ def heatmap_grad_cam(grad_cam : CNN_grad_cam, img) -> torch.Tensor:
     # allow gradients to be computed
     for param in grad_cam.features.parameters():
         param.requires_grad = True
-    pred = grad_cam(img)
-    #calculate gradients for most likely class  
-    pred[:,pred.argmax(dim=1)].backward()
-
-    gradiants = grad_cam.get_activations_gradient()
+    # get the gradiants
+    gradiants = _get_gradiants(grad_cam, img)
+    # pool gradients
     pooled_gradients = torch.mean(gradiants, dim=[0, 2, 3])
-    activations = grad_cam.get_activations(img).detach()
-    for i in range(activations.shape[1]):
-        activations[:, i, :, :] *= pooled_gradients[i]
+    #get activations
+    activations = _get_activations(grad_cam, img, pooled_gradients)
+    #construct heatmap
     heatmap = torch.mean(activations, dim=1).squeeze()
     # relu on heatmap
     heatmap = np.maximum(heatmap, 0)
@@ -72,3 +70,17 @@ def heatmap_in_image(img, heatmap, threshold, mixture) -> np.array:
     superimposed_img = (1 - image_weight) * heatmap_plotting + image_weight * img_clipped
     superimposed_img= superimposed_img / np.max(superimposed_img)
     return superimposed_img
+
+def _get_gradiants(grad_cam : CNN_grad_cam, img):
+    pred = grad_cam(img)
+    #calculate gradients for most likely class  
+    pred[:,pred.argmax(dim=1)].backward()
+    gradiants = grad_cam.get_activations_gradient()
+    return gradiants
+
+
+def _get_activations(grad_cam : CNN_grad_cam, img, pooled_gradients):
+    activations = grad_cam.get_activations(img).detach()
+    for i in range(activations.shape[1]):
+        activations[:, i, :, :] *= pooled_gradients[i]
+    return activations
