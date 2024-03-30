b9d82706e8aa5100e1b0d241f4b606f625eeb90d
Marvin Lob <marvin.lob@quantco.com>
2024-03-30 12:05:14 +0100

    improved plotting

--- models/grad_cam.py
+++ models/grad_cam.py
@@ -52,24 +52,26 @@ def heatmap_grad_cam(grad_cam : CNN_grad_cam, img) -> torch.Tensor:
     heatmap /= torch.max(heatmap)
     return heatmap
 
-def heatmap_in_image(img, heatmap, threshold, mixture) -> np.array:
-    #only keep values of the 14*14 array with value greater than 0.5
+def heatmap_in_image(img, heatmap, threshold, mixture, mn, mx) -> np.array:
+ 
     mask = heatmap > threshold
     # Apply the mask to the tensor
     heatmap_cliped = heatmap.clone()
     heatmap_cliped[~mask] = 0
     heatmap_cliped = heatmap_cliped.numpy()
     img = img.squeeze().numpy()
-    img = np.transpose(img, (1, 2, 0))
+    img = np.transpose((img - mn) / (mx - mn), (1, 2, 0))   
     heatmap_plotting = cv2.resize(heatmap_cliped, (img.shape[1], img.shape[0]))
     heatmap_plotting = np.uint8(255 * heatmap_plotting)
     # Apply a colormap to the heatmap
     heatmap_plotting = cv2.applyColorMap(heatmap_plotting, cv2.COLORMAP_HSV)/255
     img_clipped = np.clip(img, 0, 1)
+   
     image_weight = mixture
     # Blend the heatmap with the original image
+  
     superimposed_img = (1 - image_weight) * heatmap_plotting + image_weight * img_clipped
-    superimposed_img= superimposed_img / np.max(superimposed_img)
+    #superimposed_img = cv2.normalize(superimposed_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
     return superimposed_img
 
 def _get_gradiants(grad_cam : CNN_grad_cam, img):
@@ -94,12 +96,13 @@ def display_datasets_heatmap(test_grad_cam, dataloader,threshold=0.6, mixture=0.
     
     # create the subplots with reduced vertical spacing
     fig, axs = plt.subplots(num_rows, num_cols, figsize=(20, 7), gridspec_kw={'wspace': .5, 'hspace': 0.5})
-    
+    mn = min([dataloader.dataset[i][0].min() for i in range(n)]).numpy()
+    mx = max([dataloader.dataset[i][0].max() for i in range(n)]).numpy()
     for i, (img, label) in enumerate(dataloader):
         if i == n:
             break
         heatmap = heatmap_grad_cam(test_grad_cam, img)
-        joint_img = heatmap_in_image(img=img, heatmap=heatmap, threshold=threshold, mixture=mixture)
+        joint_img = heatmap_in_image(img=img, heatmap=heatmap, threshold=threshold, mixture=mixture, mn=mn, mx=mx)
         #print the prediction
         test_grad_cam.eval()
         print(test_grad_cam(img).argmax(dim=1).item(), label.item())
@@ -108,8 +111,10 @@ def display_datasets_heatmap(test_grad_cam, dataloader,threshold=0.6, mixture=0.
         col_idx = i % num_cols
         
         # plot the image in the corresponding subplot
-        axs[row_idx, col_idx].imshow(joint_img)
+        axs[row_idx, col_idx].imshow(joint_img) 
         axs[row_idx, col_idx].set_title(classes[label])
         axs[row_idx, col_idx].axis('off')
         
     plt.show()
+# Blend the heatmap with the original image
+
