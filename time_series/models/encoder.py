b5f9078c187dfb0aa817f2b7b405893084e69690
Marvin <marlob@ethz.ch>
2024-05-11 13:53:59 +0200

    fixed encodings

--- time_series/models/encoder.py
+++ time_series/models/encoder.py
@@ -22,22 +22,20 @@ class AutoEncoderCnn(torch.nn.Module):
             torch.nn.Flatten(),
             self.fc1,
         )
-        self.fc2 = nn.Linear(128, 6144)
-        self.deconv1 = nn.ConvTranspose1d(in_channels=256, out_channels=128, kernel_size=3, stride=2, padding=1)
-        self.deconv2 = nn.ConvTranspose1d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1)
-        self.deconv3 = nn.ConvTranspose1d(in_channels=64, out_channels=1, kernel_size=3, stride=2, padding=0)
-
-        self.decoder = torch.nn.Sequential(
-                    self.fc2,
-                    torch.nn.ReLU(),
-                    torch.nn.Unflatten(1, (256, 24)),
-                    self.deconv1,
-                    torch.nn.ReLU(),
-                    self.deconv2,
-                    torch.nn.ReLU(),
-                    self.deconv3,
-                    
-                )
+        self.deconv1 = nn.Conv1d(in_channels=1, out_channels=20, kernel_size=4, padding=0)
+        self.deconv2 = nn.Conv1d(in_channels=20, out_channels=20, kernel_size=4, padding=0)
+        self.deconv3 = nn.Conv1d(in_channels=20, out_channels=1, kernel_size=4, padding=1) 
+        self.decoder = nn.Sequential(
+            nn.ReLU(),
+            torch.nn.Unflatten(1, (1, 128)),    
+            self.deconv1,
+            nn.ReLU(),
+            nn.Upsample(scale_factor=1.24),  
+            self.deconv2,
+            nn.ReLU(),
+            nn.Upsample(scale_factor=1.24),
+            self.deconv3
+        )
 
     def forward(self, x):
         x = self.encoder(x)
